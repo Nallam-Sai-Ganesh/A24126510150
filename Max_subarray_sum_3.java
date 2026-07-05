64dbde1ae686bf437baba621c45c96e863356527
Nallam Sai Ganesh <papercraft271@gmail.com>
2026-07-05 14:39:26 +0530

    Maximum subarray sum using 2 and 3 loops.

--- Max_subarray_sum_3.java
+++ Max_subarray_sum_3.java
@@ -1,6 +1,6 @@
 public class Max_subarray_sum_3 {
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5};
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
